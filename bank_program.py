dc757c06e84a4e222b8155e20fa654c8fb3cb6c1
Pasha-lt <9139136@gmail.com>
2021-09-19 19:53:42 +0300

    second commit

--- bank_program.py
+++ bank_program.py
@@ -1,15 +1,30 @@
 # Напишите функцию которая будет принимать Три аргумента(Зарплата, сумма кредита и на сколько месяцев).
 # и возвращает True если кредит выдать можно и False если нет.
-# def bank():
+# def bank_count():
 #     pass
 
 def bank_count(salary, how_much,mounths):
     how_much += how_much*(mounths/10) # увеличиваем тело кредита взависимости от того на сколько месяцев.
-    print(how_much)
     living_wage_of_month = 600
     return salary - how_much/mounths >= living_wage_of_month
+
+
+# Усовершенствуйте предведущую функцию чтобы она принимала 4 аргумент затраты в месяц и
+# исходя из этого расчитывала можем ли мы выдать кредит человеку.
+# и возвращает True если кредит выдать можно и False если нет.
+# bank_count_2():
+#     pass
+def bank_count_2(salary, how_much, mounths, cost_per_mounth):
+    how_much += how_much*(mounths/10) # увеличиваем тело кредита взависимости от того на сколько месяцев.
+    print(how_much)
+    return salary - how_much/mounths >= cost_per_mounth
+
+
+print(bank_count_2(salary=1000, how_much=1000, mounths=12, cost_per_mounth=200))
 
 if __name__ == '__main__':
     assert bank_count(salary=1000, how_much=100, mounths=8) == True
     assert bank_count(salary=1000, how_much=100, mounths=4) == True
     assert bank_count(salary=1000, how_much=10000, mounths=40) == False
+    assert bank_count_2(salary=1000, how_much=1000, mounths=12, cost_per_mounth=900) == False
+    assert bank_count_2(salary=1000, how_much=3000, mounths=12, cost_per_mounth=200) == True
--- test_first.py
+++ test_first.py
@@ -14,7 +14,23 @@ def test_bank_count(salary, how_much, mounths, booltf):
     res = bank_program.bank_count(salary, how_much, mounths)
     assert res == booltf
 
-#
-# assert bank_count(salary=1000, how_much=100, mounths=8) == True
-# assert bank_count(salary=1000, how_much=100, mounths=4) == True
-# assert bank_count(salary=1000, how_much=10000, mounths=40) == False
+
+
+
+# assert bank_count_2(salary=1000, how_much=1000, mounths=12, cost_per_mounth=900) == False
+# assert bank_count_2(salary=1000, how_much=3000, mounths=12, cost_per_mounth=200) == True
+
+
+
+parametrizes_2 = [
+    (1000, 1000, 12, 900, False),
+    (1000, 3000, 12, 200, True),
+]
+
+@pytest.mark.parametrize('salary, how_much, mounths, cost_per_mounth, booltf', parametrizes_2)
+def test_bank_count(salary, how_much, mounths, cost_per_mounth, booltf):
+    res = bank_program.bank_count_2(salary, how_much, mounths, cost_per_mounth)
+    assert res == booltf
+
+
+
