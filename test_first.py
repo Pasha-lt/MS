7a96fcffa22a0e803d8a0e697a8b3b116c9cfdc1
Pasha-lt <9139136@gmail.com>
2021-09-26 22:27:38 +0300

    docs

--- test_first.py
+++ test_first.py
@@ -1,7 +1,7 @@
 import bank_program
 import pytest
 
-# Вариант с вынесением тестовых значений из декоратора.
+
 parametrizes = [
     (1000, 100, 8, True),
     (1000, 100, 4, True),
@@ -11,6 +11,7 @@ parametrizes = [
 
 @pytest.mark.parametrize('salary, how_much, mounths, booltf', parametrizes)
 def test_bank_count(salary, how_much, mounths, booltf):
+    """Вариант с вынесением тестовых значений из декоратора."""
     res = bank_program.bank_count(salary, how_much, mounths)
     assert res == booltf
 
@@ -22,33 +23,36 @@ parametrizes_2 = [
 
 @pytest.mark.parametrize('salary, how_much, mounths, cost_per_mounth, booltf', parametrizes_2)
 def test_bank_count(salary, how_much, mounths, cost_per_mounth, booltf):
+    """Вариант с вынесением тестовых значений из декоратора."""
     res = bank_program.bank_count_2(salary, how_much, mounths, cost_per_mounth)
     assert res == booltf
 
 
 
-# Тест будет пропускаться (помечатся как skip если провалиться и проходить как pass в случае выполнения).
 @pytest.mark.xfail()
 def test_xfail():
+    """Тест будет пропускаться (помечатся как skip если провалиться и проходить как pass в случае выполнения)."""
     res = sum([1,2,3])
     assert res == 6
 
 
-# Тест будет пропускаться и отображаться как skipped.
+
 @pytest.mark.skip('Причина тест сломан')
 def test_skip():
+    """Тест будет пропускаться и отображаться как skipped."""
     res = sum([1, 2, 5])
     assert res == 6
 
 
-# Тест будет пропускаться если сейчас нечетная минута.
 @pytest.mark.skipif((bank_program.minutes_now%2 == 0), reason='если сейчас нечетная минута мы запускаем этот тест', )
 def test_skipif_odd():
+    """Тест будет пропускаться если сейчас нечетная минута."""
     res = sum([2, 2, 3])
     assert res == 7
 
-# Тест будет пропускаться если сейчас четная минута.
+
 @pytest.mark.skipif((bank_program.minutes_now%2 == 1), reason='если сейчас четная минута мы запускаем этот тест', )
 def test_skipif_even():
+    """Тест будет пропускаться если сейчас четная минута."""
     res = sum([10, 2, 3])
     assert res == 15
