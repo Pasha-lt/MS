4a178cb1bc2643c4af11fc1d2864f7fd71c87248
Pasha-lt <9139136@gmail.com>
2021-10-01 22:37:58 +0300

    autouse=True

--- test_using_fixture_without teardown.py
+++ test_using_fixture_without teardown.py
@@ -9,14 +9,21 @@ class User:
     def new_title(self, title):
         self.person_new_title = title + self.name
 
+
 @pytest.fixture(scope='function')
 def user():
     my_fixture = User(name='Roman', age=30)
     return my_fixture
 
 
+# ставим autouse=True фикстура отработает даже если она не будет вызвана. По дефолту False.
+@pytest.fixture(scope='function', autouse=True)
+def create_file():
+    with open('1.txt', 'w') as f:
+        f.write('12')
+
+
 def test_user(user):
     '''В этот тест мы домешали фикстуру user'''
     user.new_title('King ')
     assert user.person_new_title == 'King Roman'
-    
